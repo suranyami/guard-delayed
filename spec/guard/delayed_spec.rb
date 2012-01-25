c554b5a86808065eac49a872c9c67932f1e89286
David Parry <david.parry@suranyami.com>
2012-01-25 11:24:05 +1100

    updated specs for fix of #2

--- spec/guard/delayed_spec.rb
+++ spec/guard/delayed_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Guard::Delayed do
   describe "when passing an environment option" do
-    it "calls system with an --environment arg" do
+    it "calls system with 'export RAILS_ENV=test;' call first" do
       @delayed = Guard::Delayed.new([], {:environment => 'test'})
-      @delayed.should_receive(:system).with("script/delayed_job --environment=test", "stop").and_return(true)
-      @delayed.should_receive(:system).with("script/delayed_job --environment=test", "start").and_return(true)
+      @delayed.should_receive(:system).with("export RAILS_ENV=test; script/delayed_job stop").and_return(true)
+      @delayed.should_receive(:system).with("export RAILS_ENV=test; script/delayed_job start").and_return(true)
       @delayed.start
     end
   end
