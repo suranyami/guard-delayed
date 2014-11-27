0dca2465ea7bb3efd461de6db071468d7b2e8ca8
David Parry <david.parry@suranyami.com>
2014-11-27 11:16:13 +1100

    fix(spec): updated spec_helper to work with new Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,18 @@
-require 'rspec'
+require 'rubygems'
 require 'guard/delayed'
+require "rspec"
 
 ENV["GUARD_ENV"] = 'test'
 
+Dir["#{File.expand_path('..', __FILE__)}/support/**/*.rb"].each { |f| require f }
+
 RSpec.configure do |config|
-  config.order = :random
-  Kernel.srand config.seed
-  config.raise_errors_for_deprecations!
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  config.before(:each) do
+    ::Guard::Notifier.stub(:notify).and_return(true)
+    @fixture_path = Pathname.new(File.expand_path('../fixtures/', __FILE__))
+  end
+
 end
